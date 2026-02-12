6e52ea184787182b1b435aa7d8ec815663610fcf
shreyashmatte <shreyashmatte@gmail.com>
2026-02-12 21:30:35 +0530

    setup app, file and folder structure routing and auth pages

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,6 @@
+import AuthForm from "@/Components/AuthForm";
+
+const Page = () => {
+    return <AuthForm type ="sign-in"/>
+}
+export default Page
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "@/Components/AuthForm";
+
+const Page = () => {
+    return <AuthForm type ="sign-in"/>
+
+}
+export default Page
